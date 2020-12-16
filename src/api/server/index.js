7175641fd6d235aa02cae3dd29af95a9f4bee846
Farinhote <andrefarinhote@gmail.com>
2020-12-16 01:45:56 +0000

    Finish request of data

--- src/api/server/index.js
+++ src/api/server/index.js
@@ -3,19 +3,25 @@ import axios from 'axios';
 const API_URL = 'https://api.openweathermap.org/data/2.5/';
 const WEATHER = 'weather';
 const ONECALL = 'onecall';
+const TIMEMACHINE = 'timemachine';
 
 axios.defaults.params = {};
 axios.defaults.params['APPID'] = process.env.VUE_APP_API_KEY;
 
+function get(url, params) {
+  return axios
+    .get(url, { params })
+    .then((response) => response.data);
+}
+
 export default {
   getWeather(params) {
-    return axios
-      .get(`${API_URL}${WEATHER}`, { params })
-      .then((response) => response.data);
+    return get(`${API_URL}${WEATHER}`, params);
   },
   getOneCallWeather(params) {
-    return axios
-      .get(`${API_URL}${ONECALL}`, { params })
-      .then((response) => response.data);
+    return get(`${API_URL}${ONECALL}`, params);
+  },
+  getHistoricalOneCallWeather(params) {
+    return get(`${API_URL}${ONECALL}/${TIMEMACHINE}`, params);
   },
 };
