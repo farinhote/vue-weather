cb9051b7388e3b0086d808502c59028c77af37da
Farinhote <andrefarinhote@gmail.com>
2020-12-17 02:51:55 +0000

    Fix rounding error

--- src/mixins/mixins.js
+++ src/mixins/mixins.js
@@ -1,8 +1,9 @@
 export default {
     methods: {
         convertTemp: function (temp, system = "metric") {
-            const celsius = temp - 273.15;
-            const fahrenheit = celsius * 9 / 5 + 32;
+            const round = (number) => Number(Math.round(number + 'e2') + 'e-2');
+            const celsius = round(temp - 273.15);
+            const fahrenheit = round(celsius * 9 / 5 + 32);
 
             return system === "metric" ? celsius : fahrenheit;
         }
