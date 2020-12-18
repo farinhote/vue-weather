784b1d1c4f556c479984bc38add12e55627da434
Farinhote <andrefarinhote@gmail.com>
2020-12-18 03:20:02 +0000

    Add check

--- src/mixins/mixins.js
+++ src/mixins/mixins.js
@@ -1,11 +1,13 @@
 export default {
     methods: {
         convertTemp: function (temp, system = "metric") {
-            const round = (number) => Number(Math.round(number + 'e2') + 'e-2');
-            const celsius = round(temp - 273.15);
-            const fahrenheit = round(celsius * 9 / 5 + 32);
+            if (temp) {
+                const round = (number) => Number(Math.round(number + 'e2') + 'e-2');
+                const celsius = round(temp - 273.15);
+                const fahrenheit = round(celsius * 9 / 5 + 32);
 
-            return system === "metric" ? celsius : fahrenheit;
+                return system === "metric" ? celsius : fahrenheit;
+            }
         }
     }
 }
