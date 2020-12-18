01cee0d9c187cf5ced6089fdd46ea3bad64d39e0
Farinhote <andrefarinhote@gmail.com>
2020-12-18 05:05:54 +0000

    Add precision to mixin

--- src/mixins/mixins.js
+++ src/mixins/mixins.js
@@ -1,8 +1,8 @@
 export default {
     methods: {
-        convertTemp: function (temp, system = "metric") {
+        convertTemp: function (temp, system = "metric", precision = 2) {
             if (temp) {
-                const round = (number) => Number(Math.round(number + 'e2') + 'e-2');
+                const round = (number) => Number(Math.round(number + `e${precision}`) + `e-${precision}`);
                 const celsius = round(temp - 273.15);
                 const fahrenheit = round(celsius * 9 / 5 + 32);
 
