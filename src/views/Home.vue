5f97a4a6c6760c1a20d98e774a3aaee24a07ee52
Farinhote <andrefarinhote@gmail.com>
2020-12-23 05:47:22 +0000

    Clean up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,14 @@
 <template>
-  <fragment>
     <Weather />
-    <!-- <Background /> -->
-    <!-- <Prevision /> -->
-  </fragment>
 </template>
 
 <script>
 import Weather from "@/components/Weather.vue";
-// import Background from "@/components/Background.vue";
-import { Fragment } from "vue-fragment";
 
 export default {
   name: "Home",
   components: {
     Weather,
-    Fragment,
   }
 };
 </script>
