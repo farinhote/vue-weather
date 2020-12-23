eee04895284feb86129da43f4eee69e3de3a2307
Farinhote <andrefarinhote@gmail.com>
2020-12-23 02:02:32 +0000

    Add weather background colors

--- src/helpers/weatherStateHelper.js
+++ src/helpers/weatherStateHelper.js
@@ -1,75 +1,75 @@
 const weatherStateHelper = {
     "01d": {
         asset: require("@/assets/clear-sky.svg"),
-        color: ""
+        color: "#DEAD4A"
     },
     "01n": {
         asset: require("@/assets/clear-sky-night.svg"),
-        color: ""
+        color: " #00799e"
     },
     "02d": {
         asset: require("@/assets/few-clouds.svg"),
-        color: ""
+        color: "#677a80"
     },
     "02n": {
         asset: require("@/assets/few-clouds-night.svg"),
-        color: ""
+        color: "#677a80"
     },
     "03d": {
         asset: require("@/assets/scattered-clouds.svg"),
-        color: ""
+        color: "#368db3"
     },
     "03n": {
         asset: require("@/assets/scattered-clouds.svg"),
-        color: ""
+        color: "#368db3"
     },
     "04d": {
         asset: require("@/assets/broken-clouds.svg"),
-        color: ""
+        color: "#286f8d"
     },
     "04n": {
         asset: require("@/assets/broken-clouds.svg"),
-        color: ""
+        color: "#286f8d"
     },
     "09d": {
         asset: require("@/assets/rain.svg"),
-        color: ""
+        color: "#286f8d"
     },
     "09n": {
         asset: require("@/assets/rain.svg"),
-        color: ""
+        color: "#286f8d"
     },
     "10d": {
         asset: require("@/assets/rain.svg"),
-        color: ""
+        color: "#286f8d"
     },
     "10n": {
         asset: require("@/assets/rain.svg"),
-        color: ""
+        color: "#286f8d"
     },
     "11d": {
         asset: require("@/assets/storm.svg"),
-        color: ""
+        color: "#286f8d"
     },
     "11n": {
         asset: require("@/assets/storm.svg"),
-        color: ""
+        color: "#286f8d"
     },
     "13d": {
         asset: require("@/assets/snow.svg"),
-        color: ""
+        color: "#438da5"
     },
     "13n": {
         asset: require("@/assets/snow.svg"),
-        color: ""
+        color: "#438da5"
     },
     "50d": {
         asset: require("@/assets/mist.svg"),
-        color: ""
+        color: "#a3afb3"
     },
     "50n": {
         asset: require("@/assets/mist.svg"),
-        color: ""
+        color: "#a3afb3"
     },
 };
 
