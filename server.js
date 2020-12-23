360c1557da7eeac7f13b914c976bade6a100f4c1
Farinhote <andrefarinhote@gmail.com>
2020-12-23 01:43:54 +0000

    Change let to const

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const serveStatic = require("serve-static")
 
 const path = require('path');
 
-let app = express();
+const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 80;
