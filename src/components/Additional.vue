de9191fc80cdc2627e2cae9f309a466201a4b60a
Farinhote <andrefarinhote@gmail.com>
2020-12-23 04:21:03 +0000

    Change layout of cards

--- src/components/Additional.vue
+++ src/components/Additional.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="card-lists">
     <!-- Prevent undefined errors while still loading -->
-    <Card-list v-if="history.length === 5" :list="history" :title="`Last 5 days`"/>
-    <Card-list :list="forecast" :title="`Forecast`" />
+    <Card-list v-if="forecast.length" :list="forecast" :title="`Forecast`" />
+    <Card-list
+      v-if="history.length === 5"
+      :list="history"
+      :title="`Last 5 days`"
+    />
   </div>
 </template>
 
 <script>
-
 import CardList from "@/components/CardList.vue";
 
 export default {
   name: "Additional",
   components: {
-    CardList
+    CardList,
   },
 
   computed: {
@@ -30,13 +33,7 @@ export default {
 <style scoped lang="scss">
 .card-lists {
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-}
-
-@media (max-width: 1080px) {
-  .card-lists {
-    grid-template-columns: 1fr;
-    grid-gap: 2rem;
-  }
+  grid-template-columns: 1fr;
+  grid-gap: 2rem;
 }
 </style>
